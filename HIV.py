168c3a03c475502ec4e75eeae2df700f3580a685
Faith Montemayor <faith.montemayor@ucdenver.edu>
2023-04-07 10:51:49 -0600

    HIV.py is used to play around with the parameters of the viral load

--- HIV.py
+++ HIV.py
@@ -1,9 +1,32 @@
-# -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Fri Mar 31 10:45:41 2023
 
-This is a temporary script file.
+@author: faithmontemayor
 """
 import numpy as np
 import matplotlib.pyplot as plt
+
+# The equation is 
+# V(t)=Aexp(-alpha *t)+Bexp(-beta*t)
+# t is the time in days
+# V is the viral load
+time = np.linspace(0,1,101)
+A = 10000
+B = 90000
+alpha = 100
+beta = 0.1
+
+#Calculate the viral load
+viral_load = A * np.exp(-alpha*time) + B * np.exp(-beta*time)
+
+# plot viral load
+Astring = 'A='+str(A)+'\n'
+Bstring = 'B='+str(B)+'\n'
+alphastring = 'alpha='+str(alpha)+'\n'
+betastring = 'beta='+str(beta)+'\n'
+sample = Astring + Bstring + alphastring + betastring
+plt.plot(time,viral_load,label=sample)
+plt.xlabel('Time (days)')
+plt.ylabel('Viral Load')
+plt.legend()
 
